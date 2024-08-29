240ec88ed5030d8c32d416febc7c7432cf36e48d
sanjeetgit1 <sksanjeet616@gmail.com>
2024-08-29 21:00:22 +0530

    all

--- src/index.js
+++ src/index.js
@@ -3,20 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider}  from "react-redux";
 import store from './Store';
-// import {  positions, transitions } from "react-alert";
+import {  positions, transitions } from "react-alert";
 
 
-// const options = {
-//   position: positions.BOTTOM_CENTER,
-//   timeout: 5000,
-//   transition: transitions.SCALE,
-// };
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  transition: transitions.SCALE,
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
  <Provider store={store}>
-     
         <App />
       
     </Provider> 
