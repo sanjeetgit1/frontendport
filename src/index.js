7fac2f340d677b2fde3134624c58a1aa440fdc22
sanjeetgit1 <sksanjeet616@gmail.com>
2024-08-29 20:33:23 +0530

    second

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider}  from "react-redux";
 import store from './Store';
-import {  positions, transitions } from "react-alert";
+// import {  positions, transitions } from "react-alert";
 
 
-const options = {
-  position: positions.BOTTOM_CENTER,
-  timeout: 5000,
-  transition: transitions.SCALE,
-};
+// const options = {
+//   position: positions.BOTTOM_CENTER,
+//   timeout: 5000,
+//   transition: transitions.SCALE,
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
